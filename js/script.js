681128c2d2869b705f3ae12ad6bd5963e4353704
Robert <Robert.Tobin@gmx.com>
2021-01-09 00:28:55 +0000

    00:28

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-$(document).on("scroll", function() {
-  var pageTop = $(document).scrollTop();
-  var pageBottom = pageTop + $(window).height();
-  var tags = $(".textmove");
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".textmove");
 
-  for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
 
-    if ($(tag).position().top < pageBottom) {
-      $(tag).addClass("visible");
-    } else {
-      $(tag).removeClass("visible");
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
     }
-  }
 });
 
-$(document).on("scroll", function() {
-  var pageTop = $(document).scrollTop();
-  var pageBottom = pageTop + $(window).height();
-  var tags = $(".textmovetime");
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".textmovetime");
 
-  for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
 
-    if ($(tag).position().top < pageBottom) {
-      $(tag).addClass("visible");
-    } else {
-      $(tag).removeClass("visible");
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
     }
-  }
 });
 
 // add padding top to show content behind navbar
@@ -36,18 +36,40 @@ $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 // detect scroll top or down
 if ($('.smart-scroll').length > 0) { // check if element exists
     var last_scroll_top = 0;
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top) {
+        if (scroll_top < last_scroll_top) {
             $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-        }
-        else {
+        } else {
             $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
         }
         last_scroll_top = scroll_top;
     });
 }
 
-    $(".img-fluid").addClass("wow fadeIn z-depth-1-half");
+$(".img-fluid").addClass("wow fadeIn z-depth-1-half");
 
-    new WOW().init();
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+    // Do something on scroll
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if ((rootElement.scrollTop / scrollTotal) > 0.99) {
+        // Show button
+        scrollToTopBtn.classList.add("showBtn")
+    } else {
+        // Hide button
+        scrollToTopBtn.classList.remove("showBtn")
+    }
+}
+
+function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
